79c4baf369a9bf78dae71fdfdd801beb6c7ffb93
Ivo La Puma <ivolapuma@gmail.com>
2021-07-08 12:17:38 -0300

    exemplos de operadores e strings

---
+++ 02-operadores.rb
@@ -0,0 +1,26 @@
+puts 3 + 2  # operador de adição
+puts 3 - 2  # operador de subtração
+puts 3 * 2  # operador de multiplicação
+
+puts 3 / 2  # operador de divisão
+puts 3 / 2.0  # operador de divisão
+
+puts 3 ** 2 # operador de exponenciação
+
+# usamos parenteses para definir a prioridade de operações, caso contrário, 
+# vale a regra da Matemática
+puts 1 + 2 * 3 - 4 / 5
+puts (1 + 2) * 3 - 4 / 5
+
+puts 5 / 2 
+puts 5 % 2  # o resto da divisão entre 5 e 2
+puts 5.0 % 2.0  # o resto da divisão entre 5 e 2
+
+puts 1250.0 / 100 * 70
+
+puts 9 ** (0.5)
+
+# está sendo chamada a função sqrt() da biblioteca Math, que é forneceida no Ruby
+puts Math.sqrt(9) 
+
+
---
+++ 03-string.rb
@@ -0,0 +1,34 @@
+
+# exemplos de string
+puts "este texto eh uma string"
+puts "uma string eh tudo que está entre aspas"
+puts 'entre aspas duplas outra simples, como esse exemplo'
+
+puts "caixa d'água"
+puts 'caixa d\'água'
+
+puts "então, ele disse: \"Olá!\""
+
+
+# operadores de strings
+puts "Olá," + " mundo"  # com strings, o operador + vai fazer uma concatenação
+# com strings, o operador + vai fazer uma concatenação
+puts "Olá," + " mundo" + "!" + "E mais coisas que eu quiser concatenar" 
+
+puts "1" + "2"  # a operação é concatenação de strings
+puts 1 + 2      # a operação é soma entre números
+
+# puts "1 + 2"
+# puts 1 + 2
+
+# puts "1 + 2 = 3"
+
+# por ora, to_s é o comando do Ruby que converte valores para o formato string
+puts "1 + 2 = " + (1 + 2).to_s
+
+# operador * com string faz uma repetição de concatenações
+puts "9" * 3
+puts "ABC" * 10
+
+
+
