de9e4785c535d4fe625a023fa731dd1f21a84efd
Ivo La Puma <ivolapuma@gmail.com>
2021-07-08 16:01:47 -0300

    adicionando resolucao exercicio 01

---
+++ exercicios/01-exercicio-resolvido.rb
@@ -0,0 +1,20 @@
+
+# quantas horas há em um ano
+puts 1 * 365 * 24
+
+# quantos minutos há em uma década
+puts 1 * 10 * 365 * 24 * 60
+
+# qual é a sua idade em segundos
+puts 40 * 365 * 24 * 60 * 60
+
+# quantos chocolates você pretende comer na vida
+puts 5 * 12 * 200
+
+# se minha idade é de 1232 milhões de segundos, qual é minha idade em anos
+puts 1232000000 / 60 / 60 / 24 / 365
+puts 1232000000.0 / 60 / 60 / 24 / 365
+
+# qtde de segundos em 1 ano
+puts 1232000000.0 / (1 * 365 * 24 * 60 * 60)
+
